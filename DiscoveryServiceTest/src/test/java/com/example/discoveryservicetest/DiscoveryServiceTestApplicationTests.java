e5d17a9413cc2ae2f34f9027b389b42a80b3bfe2
YunHo Choi <svs101@dgu.ac.kr>
2022-07-21 23:51:24 +0900

    First commit

---
+++ DiscoveryServiceTest/src/test/java/com/example/discoveryservicetest/DiscoveryServiceTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.discoveryservicetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
